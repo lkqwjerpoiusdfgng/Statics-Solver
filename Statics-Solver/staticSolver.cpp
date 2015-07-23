e8259046bbd42a94540791756f01b6c5e9d20648
PR0ficiency <medlin.craig@gmail.com>
2015-07-23 12:46:14 -0500

    Added description of staticSolver class. Made constructor frameworks. Will now spend time working on planning and design of program logic on paper.

---
+++ Statics-Solver/staticSolver.cpp
@@ -0,0 +1,21 @@
+#include "staticSolver.h"
+#include <string>
+
+
+/* CONSTRUCTORS */
+staticSolver::staticSolver()
+{
+
+}
+
+staticSolver::staticSolver(std::string probDimens)
+{
+
+}
+
+
+/* DESTRUCTOR */
+staticSolver::~staticSolver()
+{
+
+}
--- Statics-Solver/staticSolver.h
+++ Statics-Solver/staticSolver.h
@@ -1,7 +1,30 @@
+/***************************************
+ * The purpose of the staticSolver class
+ * is to lay the framework for solving
+ * statics problems. It will obtain 
+ * initial information from the user
+ * regarding the problem to be solved
+ * and orchestrate the creation and 
+ * management of the necessary objects
+ * to solve the problems.
+ */
+
+
 #ifndef STATICSOLVER_H
 #define STATICSOLVER_H
 
+#include <string>
 
+class staticSolver {
+
+	public:
+		/* Constructors */
+		staticSolver();
+		staticSolver(std::string);
+
+		/* Destructor */
+		~staticSolver();
+};
 
 
 #endif
